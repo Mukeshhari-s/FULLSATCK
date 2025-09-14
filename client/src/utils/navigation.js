705eac32fc3a0a819f70754d72433ff8fd5667f2
Mukeshhari-s <mukeshharisk90@gmail.com>
2025-09-14 21:54:41 +0530

    review and admin board has been changed

--- client/src/utils/navigation.js
+++
@@ -1,9 +0,0 @@
-export const saveReturnTo = (path) => {
-  sessionStorage.setItem('returnTo', path);
-};
-
-export const getReturnTo = () => {
-  const returnTo = sessionStorage.getItem('returnTo');
-  sessionStorage.removeItem('returnTo'); // Clear it after getting it
-  return returnTo || '/';
-};
